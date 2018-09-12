127eb1c6d1cf481b1a3a695cb91b69bdb7c13745
dukecg@gmail.com <dukecg@gmail.com>
2018-09-12 18:04:07 +1000

    Added HAPI caller

---
+++ Source/HoudiniPlusEditor/Private/HAPI.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HAPI.h"
+
+HAPI::HAPI()
+{
+}
+
+HAPI::~HAPI()
+{
+}
---
+++ Source/HoudiniPlusEditor/Public/HAPI.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class HOUDINIPLUSEDITOR_API HAPI
+{
+public:
+	HAPI();
+	~HAPI();
+};
